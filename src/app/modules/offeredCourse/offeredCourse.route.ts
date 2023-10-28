ddc6026af5c1b37bde61dfb8c00abca474fca6a6
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-10-28 07:20:53 +0600

    Integration of Zod Validation for Offered Course

--- src/app/modules/offeredCourse/offeredCourse.route.ts
+++ src/app/modules/offeredCourse/offeredCourse.route.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import validateRequest from '../../middlewares/validateRequest';
 import { offeredCourseController } from './offeredCourse.controller';
+import { OfferedCourseValidations } from './offeredCourse.validation';
 
 const router = express.Router();
 
 // create
 router.post(
   '/create-offered-course',
+  validateRequest(OfferedCourseValidations.offeredZodSchema),
   offeredCourseController.createOfferedCourse
 );
 
